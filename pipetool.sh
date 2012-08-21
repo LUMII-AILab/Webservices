399d8bbaa89c3aa156f7b14f9b7cd4075798b9f4
Pēteris Paikens <PeterisP@gmail.com>
2012-08-21 15:28:35 +0300

    Folder name fix

--- pipetool.sh
+++ pipetool.sh
@@ -1,2 +1,1 @@
-cd /Users/pet/Documents/java/Webservices/
 java -Xmx2G -cp dist/webservices.jar:dist/morphology.jar:dist/transliterator.jar:lib/json_simple-1.1.jar:lib/commons-lang3-3.1.jar lv.semti.morphology.pipetool.WordPipe $*
