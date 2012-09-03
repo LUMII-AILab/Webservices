72cd589ff562aae7a30bf360e779f2d6dfeeb436
Pēteris Paikens <PeterisP@gmail.com>
2012-09-03 17:15:44 +0300

    shellscript fix

--- pipetool.sh
+++ pipetool.sh
@@ -1,1 +1,4 @@
+#!/bin/bash
+
+cd $(dirname $0)
 java -Xmx2G -cp dist/webservices.jar:dist/morphology.jar:dist/transliterator.jar:lib/json_simple-1.1.jar:lib/commons-lang3-3.1.jar lv.semti.morphology.pipetool.WordPipe $*
