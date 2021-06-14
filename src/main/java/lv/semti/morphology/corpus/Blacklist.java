63b5270e96cac35a162cb8999989f6b9c1bd83da
Lauma <lauma.pretkalnina@gmail.com>
2021-06-14 12:02:07 +0300

    Spektora ieziņotie sliktie piemēri

--- src/main/java/lv/semti/morphology/corpus/Blacklist.java
+++ src/main/java/lv/semti/morphology/corpus/Blacklist.java
@@ -13,7 +13,14 @@ public class Blacklist {
 
     private static void createBlacklist() {
         blacklist_singleton = new HashMap<>();
+        blacklist_singleton.put("aloties", new LinkedList<String>(){{add("Aloj");}} );
+        blacklist_singleton.put("cērps", new LinkedList<String>(){{add("mēs cērpam katrs");}} );
         blacklist_singleton.put("dervišs", new LinkedList<String>(){{add("Eroglu");}} );
+        blacklist_singleton.put("irt", new LinkedList<String>(){{add("irtā pretruna"); add("raugs irtas"); add("tie irtie");}} );
+        blacklist_singleton.put("sadars", new LinkedList<String>(){{add("");}} );
+        blacklist_singleton.put("trops", new LinkedList<String>(){{add("Inga Tropa");}} );
+        blacklist_singleton.put("vēlēt", new LinkedList<String>(){{add("Vēl viens faktors"); add("Vēl vairāk nekā"); add("vēl vairāk nekā"); add("zeme vēl tālu");}} );
+        blacklist_singleton.put("vērst", new LinkedList<String>(){{add("spītīgs vērsis");}} );
     }
 
     public static boolean is_blacklisted(String sentence, String lemma) {
