7d6ab311b0605574879d5f86eccebbcc9dae8311
Pēteris Paikens <peterisp@gmail.com>
2024-05-15 02:42:14 +0300

    Functionality to reload lexicon from Tezaurs DB

--- src/main/java/lv/semti/morphology/webservice/ReloadLexiconResource.java
+++ src/main/java/lv/semti/morphology/webservice/ReloadLexiconResource.java
@@ -4,17 +4,18 @@ import lv.semti.morphology.analyzer.Analyzer;
 import org.restlet.resource.Post;
 import org.restlet.resource.ServerResource;
 
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
+import java.io.*;
 import java.net.URLDecoder;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.util.Date;
 import java.util.concurrent.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 class Reloader {
-	private static String TEZAURS_DUMP_PATH = "../tezaurs_dump/tezaurs_dump.py";
-//	private static String TEZAURS_DUMP_PATH = "python3 tezaurs_dump/tezaurs_dump.py";
+	private static String TEZAURS_DUMP_PATH = "../tezaurs_dump/";
+//	private static String TEZAURS_DUMP_PATH = "python3 tezaurs_dump/";
 
 	private static Date reloadStart = null;
 	public static synchronized Date getReloadStart() { return reloadStart; }
@@ -46,42 +47,113 @@ class Reloader {
 		return null;
 	}
 
-	private static void reload() {
+	private static void reload(){
 		System.out.println("Starting reload at " + new Date());
 		try {
 			// Check if the file exists
-			if (!Files.exists(Paths.get(TEZAURS_DUMP_PATH))) {
-				System.err.println("Tezaurs dump file does not exist: " + TEZAURS_DUMP_PATH);
+
+			String script_path = TEZAURS_DUMP_PATH+"tezaurs_dump.py";
+			if (!Files.exists(Paths.get(script_path))) {
+				System.err.println("Tezaurs dump script does not exist: " + script_path);
 				return;
 			}
 			// Create ProcessBuilder with command and arguments
-			ProcessBuilder processBuilder = new ProcessBuilder("python", TEZAURS_DUMP_PATH);
+			ProcessBuilder processBuilder = new ProcessBuilder("python3", script_path);
 			// Start the process
 			Process process = processBuilder.start();
-			// Wait for the process to finish
+
+			// Create ExecutorService for reading subprocess output
+			ExecutorService executor = Executors.newFixedThreadPool(2);
+			// Submit tasks to read stdout and stderr asynchronously
+			Future<Void> stdoutFuture = executor.submit(() -> {
+				readStream(process.getInputStream(), null);
+				return null;
+			});
+			Future<Void> stderrFuture = executor.submit(() -> {
+				readStream(process.getErrorStream(), System.err);
+				return null;
+			});
+
+			// Wait for the process to finish and the stdout and stderr reading tasks to complete
 			int exitCode = process.waitFor();
+			stdoutFuture.get();
+			stderrFuture.get();
 
 			// Check if the process exited normally
 			if (exitCode != 0) {
-				System.err.println("Script execution failed with exit code: " + exitCode);
+				System.err.println("Failed to execute script " + script_path + " with exit code: " + exitCode);
+				System.out.println("Ending reload at " + new Date());
 				return;
 			}
 
 			// At this point we assume that the lexicon JSON files are written somewhere and we need to load them
-			// FIXME / TODO
-//
-//			Analyzer = new Analyzer(false);
-//			analyzer.setCacheSize(1000);
-//
-//			if (enableLatgalian) {
-//				latgalian_analyzer = new Analyzer("Latgalian.xml", false);
-//				latgalian_analyzer.setCacheSize(100);
-//			}
-		} catch (IOException | InterruptedException e) {
+			System.out.println("DB extract done at " + new Date());
+
+			// Copying all the required files from the dump and also from the .jar file to the same resources/ folder
+			Path sourcePath = Paths.get( "tezaurs_lexemes.json");
+			if (!Files.exists(sourcePath)) {
+				System.err.println("DB extract result file not found " + sourcePath);
+				return;
+			}
+			Path targetPath = Paths.get("resources/").resolve(sourcePath.getFileName());
+			Files.move(sourcePath, targetPath, StandardCopyOption.REPLACE_EXISTING);
+
+			sourcePath = Paths.get( "tezaurs_latgalian.json");
+			if (!Files.exists(sourcePath)) {
+				System.err.println("DB extract result file not found " + sourcePath);
+				return;
+			}
+			targetPath = Paths.get("resources/").resolve(sourcePath.getFileName());
+			Files.move(sourcePath, targetPath, StandardCopyOption.REPLACE_EXISTING);
+
+			extractResourceFromJAR("Lexicon_v2.xml");
+			extractResourceFromJAR("Lexicon_minicore.xml");
+			extractResourceFromJAR("Lexicon_firstnames.xml");
+			extractResourceFromJAR("Lexicon_vietas.xml");
+			extractResourceFromJAR("Latgalian.xml");
+			extractResourceFromJAR("Latgalian_minicore.xml");
+
+			Analyzer analyzer = new Analyzer("resources/Lexicon_v2.xml",false);
+			analyzer.setCacheSize(1000);
+			MorphoServer.setAnalyzer(analyzer);
+			if (MorphoServer.enableLatgalian) {
+				Analyzer latgalian_analyzer = new Analyzer("Latgalian.xml", false);
+				latgalian_analyzer.setCacheSize(100);
+				MorphoServer.setLatgalian_analyzer(latgalian_analyzer);
+			}
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		System.out.println("Ending reload at " + new Date());
+	}
+
+	private static void readStream(InputStream inputStream, PrintStream output) {
+		try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+			String line;
+			while ((line = reader.readLine()) != null) {
+				if (output != null)
+					output.println(line);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void extractResourceFromJAR(String filename) throws IOException {
+		// Read the resource file from the JAR
+		InputStream inputStream = Reloader.class.getClassLoader().getResourceAsStream(filename);
+		if (inputStream == null) {
+			throw new IOException("Resource file not found: " + filename);
+		}
+		// Create a new file in the local working directory
+		OutputStream outputStream = new FileOutputStream("resources/"+filename);
+		byte[] buffer = new byte[1024];
+		int bytesRead;
+		while ((bytesRead = inputStream.read(buffer)) != -1) {
+			outputStream.write(buffer, 0, bytesRead);
+		}
 	}
 }
 
